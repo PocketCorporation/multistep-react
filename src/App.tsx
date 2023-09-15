3c3eeda7548cb569adcf7102e8f9a982e84c9157
pocketcorp <pocketcorp@protonmail.com>
2023-09-14 21:50:11 -0700

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ type FormData = {
   firstName: string
   lastName: string
   age: string
+  street: string
   city: string
   state: string
   zip: string
@@ -20,6 +21,7 @@ const INITIAL_DATA = {
   firstName: '',
   lastName: '',
   age: '',
+  street: '',
   city: '',
   state: '',
   zip: '',
@@ -29,13 +31,22 @@ const INITIAL_DATA = {
 
 function App() {
   const [data, setData] = useState(INITIAL_DATA)
+  function updateFields(fields: Partial<FormData>){
+    setData(prev => {
+      return {...prev, ...fields}
+    })
+  }
+
   const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } = useMultistepForm([
-    <UserForm />, <AddressForm />, <AccountForm />
+    <UserForm {...data} updateFields={updateFields}/>,
+    <AddressForm {...data} updateFields={updateFields}/>,
+    <AccountForm {...data} updateFields={updateFields}/>,
   ])
 
   function onSubmit(e: FormEvent) {
     e.preventDefault()
-    next()
+    if (!isLastStep) return next()
+    alert('Successful Account Creation')
   }
   return <div style={{
     position: 'relative',
@@ -44,7 +55,8 @@ function App() {
     padding: '2rem',
     margin: '1rem',
     borderRadius: '.5rem',
-    fontFamily: 'Arial'
+    fontFamily: 'Arial',
+    // maxWidth: 'max-content'
   }}>
     <form onSubmit={onSubmit}>
       <div style={{ position: 'absolute', top: '.5rem', right: '.5rem'}}>
